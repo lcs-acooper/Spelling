01c612f8e777b80fd3bbdaab8094000cd75429de
AJ Cooper <acooper@lcs.on.ca>
2025-03-28 09:06:34 -0400

    User can now make a guess with feedback on answer

--- Spelling/Views/QuizView.swift
+++ Spelling/Views/QuizView.swift
@@ -12,6 +12,9 @@ struct QuizView: View {
     // MARK: Stored properties
     @State var currentItem = itemsToSpell.randomElement()!
     
+    // The user's guess
+    @State var userGuess = ""
+    
     // MARK: Computed properties
     var body: some View {
         
@@ -19,7 +22,28 @@ struct QuizView: View {
             Image(currentItem.imageName)
                 .resizable()
                 .scaledToFit()
+            
+            TextField("Enter the name of the item", text: $userGuess)
+            
+            Button {
+                checkGuess()
+            } label: {
+                Text("Submit")
+                }
+            
         }
+        
+    }
+    
+    // MARK: Functions
+    func checkGuess() {
+        
+        if userGuess == currentItem.word {
+            print("Correct")
+        } else {
+            print("Incorrect")
+        }
+            
         
     }
 }
